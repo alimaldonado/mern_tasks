547d135f4c711ca89dcfb9a830dfb78be6dcd8b8
Ali Maldonado <alimaldonado.1995@gmail.com>
2021-01-05 16:50:00 -0700

    created connection to database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require('express');
+const conectDB = require('./config/db');
+
+const app = express();
+
+conectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.use('/api/users', require('./routes/users'));
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
